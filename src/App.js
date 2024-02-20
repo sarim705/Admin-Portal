f1210323f8bf5f1acd696a826b5d0b0edc914576
sarim705 <sarimiqbal705@gmail.com>
2024-02-20 18:41:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Container, Paper, TextField, Button, IconButton } from '@mui/material';
+import UsersTable from './Users/UsersTable';  
+import axios from 'axios';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [editingRow, setEditingRow] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
+        console.log('Data from API:', response.data);
+        setUsers(response.data);
+        setFilteredUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Error fetching data. Please check the console for details.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSearch = () => {
+    const filtered = users.filter(user =>
+      Object.values(user).some(value =>
+        value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+    setFilteredUsers(filtered);
+    setCurrentPage(1);
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleCheckboxChange = (event, id) => {
+    if (event.target.checked) {
+      setSelectedRows([...selectedRows, id]);
+    } else {
+      setSelectedRows(selectedRows.filter(rowId => rowId !== id));
+    }
+  };
+
+  const handleSelectAllRows = () => {
+    const allIds = filteredUsers.slice((currentPage - 1) * 10, currentPage * 10).map(user => user.id);
+    setSelectedRows(allIds);
+  };
+
+  const handleEditRow = (id) => {
+    setEditingRow(id);
+  };
+
+  const handleSaveEdit = (id, updatedName, updatedEmail) => {
+    const updatedUsers = users.map(user => {
+      if (user.id === id) {
+        return {
+          ...user,
+          name: updatedName,
+          email: updatedEmail,
+        };
+      }
+      return user;
+    });
+    setUsers(updatedUsers);
+    setFilteredUsers(updatedUsers);
+    setEditingRow(null);
+  };
+
+  const handleDeleteRow = (id) => {
+    const updatedUsers = users.filter(user => user.id !== id);
+    setUsers(updatedUsers);
+    setFilteredUsers(updatedUsers);
+    setSelectedRows(selectedRows.filter(rowId => rowId !== id));
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Paper>
+        <TextField
+          label="Search"
+          variant="outlined"
+          placeholder="Enter..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <Button variant="contained" onClick={handleSearch}>
+          Search
+        </Button>
+
+        <UsersTable
+          users={filteredUsers}
+          currentPage={currentPage}
+          setCurrentPage={handlePageChange}
+          selectedRows={selectedRows}
+          onCheckboxChange={handleCheckboxChange}
+          onSelectAllRows={handleSelectAllRows}
+          onEditRow={handleEditRow}
+          editingRow={editingRow}
+          onSaveEdit={handleSaveEdit}
+          onDeleteRow={handleDeleteRow} // Passing handleDeleteRow function
+        />
+
+        {selectedRows.length > 0 && (
+          <IconButton onClick={() => handleDeleteRow(selectedRows[0])}>
+            <DeleteIcon />
+          </IconButton>
+        )}
+      </Paper>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/Users/UsersTable.js
@@ -0,0 +1,133 @@
+import React, { useState } from 'react';
+import { Table, TableBody, TableRow, TableCell, TableHead, TableContainer, Paper, Checkbox, IconButton, TextField, Button } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const UsersTable = ({ users = [], currentPage, setCurrentPage, selectedRows = [], onCheckboxChange, onSelectAllRows, onEditRow, onSaveEdit, editingRow, onDeleteRow }) => {
+
+  const itemsPerPage = 8;
+  const totalPages = Math.ceil(users.length / itemsPerPage);
+
+  const [editedName, setEditedName] = useState('');
+  const [editedEmail, setEditedEmail] = useState('');
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleCheckboxChange = (event, id) => {
+    onCheckboxChange(event, id);
+  };
+
+  const handleSelectAllRows = () => {
+    onSelectAllRows();
+  };
+
+  const handleEditRow = (id, name, email) => {
+    onEditRow(id);
+    setEditedName(name);
+    setEditedEmail(email);
+  };
+
+  const handleSaveEdit = (id) => {
+    onSaveEdit(id, editedName, editedEmail);
+    onEditRow(null);
+    setEditedName('');
+    setEditedEmail('');
+  };
+
+  const handleDeleteRow = (id) => {
+    onDeleteRow(id);
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              <Checkbox
+                onChange={handleSelectAllRows}
+                indeterminate={selectedRows.length > 0 && selectedRows.length < itemsPerPage}
+                checked={selectedRows.length === itemsPerPage}
+              />
+            </TableCell>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users
+            .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+            .map((user) => (
+              <TableRow key={user.id} style={{ background: selectedRows.includes(user.id) ? '#c1c1c1' : '#e3f2fd' }}>
+                <TableCell>
+                  <Checkbox
+                    onChange={(event) => handleCheckboxChange(event, user.id)}
+                    checked={selectedRows.includes(user.id)}
+                  />
+                </TableCell>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>
+                  {editingRow === user.id ? (
+                    <TextField
+                      value={editedName}
+                      onChange={(e) => setEditedName(e.target.value)}
+                    />
+                  ) : (
+                    user.name
+                  )}
+                </TableCell>
+                <TableCell>
+                  {editingRow === user.id ? (
+                    <TextField
+                      value={editedEmail}
+                      onChange={(e) => setEditedEmail(e.target.value)}
+                    />
+                  ) : (
+                    user.email
+                  )}
+                </TableCell>
+                <TableCell>
+                  {editingRow === user.id ? (
+                    <Button variant="contained" color='secondary' onClick={() => handleSaveEdit(user.id)}>
+                      Save
+                    </Button>
+                  ) : (
+                    <Button variant="outlined" onClick={() => handleEditRow(user.id, user.name, user.email)}>
+                      Edit
+                    </Button>
+                  )}
+                  {selectedRows.includes(user.id) && (
+                    <IconButton onClick={() => handleDeleteRow(user.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  )}
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+
+      
+      <div>
+        <Button variant='contained' onClick={() => handlePageChange(1)} disabled={currentPage === 1}>
+          First Page
+        </Button>
+        <Button variant='contained' color="secondary" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+          Previous
+        </Button>
+        <span> Page {currentPage} of {totalPages} </span>
+        <Button variant='contained' color="primary" onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+          Next Page
+        </Button>
+        <Button variant='contained' color="secondary" onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages}>
+          Last Page
+        </Button>
+      </div>
+    </TableContainer>
+  );
+};
+
+export default UsersTable;
